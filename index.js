fdc0f11aaaeaa1079064a923a8f091c9c369a2a8
Alexey Novikov <velocityzen@gmail.com>
2015-03-18 15:55:59 -0400

    New plugin api

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
 "use strict";
-let through2 = require("through2");
 let autoprefixer = require("autoprefixer-core");
 
-let Autoprefixer = function(fuller, options) {
-	fuller.bind(this);
-	this.processor = autoprefixer({browsers: options.browsers || "last 2 versions"});
+module.exports = function(f, mat, options, next) {
+	mat.getContent(function(content) {
+		let processor = autoprefixer({browsers: options.browsers || "last 2 versions"});
+		let newContent = processor.process( content.toString() ).css;
+		next(null, mat.setContent(newContent));
+	});
 };
-
-Autoprefixer.prototype.compile = function(css, cb) {
-	cb(null, this.processor(css));
-};
-
-Autoprefixer.prototype.build = function(stream) {
-	if(!this.compress) { return stream;}
-
-	let self = this,
-		buffer = [];
-
-	console.log(stream);
-	return stream.pipe( through2(
-		function(chunk, enc, cb) {
-			buffer.push(chunk);
-			cb();
-		},
-		function(cb) {
-			let that = this;
-			self.compile(buffer.join(""), function(err, result) {
-				!err && that.push(result);
-				cb();
-			});
-		}
-	));
-};
-
-
-module.exports = Autoprefixer;
